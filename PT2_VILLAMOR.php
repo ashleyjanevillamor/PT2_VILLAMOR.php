bce6f879cc610d936f18afbc97de5ebb6fcd58da
ashleyjanevillamor <villamor.ashleyjane@ici.edu.ph>
2025-02-14 18:12:58 +0800

    solving php total cost

--- PT2_VILLAMOR.php
+++ PT2_VILLAMOR.php
@@ -66,6 +66,64 @@ p {
   $total_cost2 = $Lcost + $Lcost * 0.15;
   $total_cost3 = $Lcost + $Lcost * 0.20;
 
+  $Increase1 = 0.1;
+  $Increase2 = 0.15;
+  $Increase3 = 0.20;
+  $Lcost = 150000;
+  $Ccost = 78000;
+  $Dcost = 69000;
+  $Pcost = 12000;
+  $Mcost = 20000;
+
+  $total_cost4 = $Ccost + $Ccost * 0.1;
+  $total_cost5 = $Ccost + $Ccost * 0.15;
+  $total_cost6 = $Ccost + $Ccost * 0.20;
+
+  $Increase1 = 0.1;
+  $Increase2 = 0.15;
+  $Increase3 = 0.20;
+  $Lcost = 150000;
+  $Cocost = 78000;
+  $Dcost = 69000;
+  $Pcost = 12000;
+  $Mcost = 20000;
+
+  $total_cost7 = $Dcost + $Dcost * 0.1;
+  $total_cost8 = $Dcost + $Dcost * 0.15;
+  $total_cost9 = $Dcost + $Dcost * 0.20;
+
+  $Increase1 = 0.1;
+  $Increase2 = 0.15;
+  $Increase3 = 0.20;
+  $Lcost = 150000;
+  $Cocost = 78000;
+  $Dcost = 69000;
+  $Pcost = 12000;
+  $Mcost = 20000;
+
+  $total_cost10 = $Pcost + $Pcost * 0.1;
+  $total_cost11 = $Pcost + $Pcost * 0.15;
+  $total_cost12 = $Pcost + $Pcost * 0.20;
+
+  $Increase1 = 0.1;
+  $Increase2 = 0.15;
+  $Increase3 = 0.20;
+  $Lcost = 150000;
+  $Cocost = 78000;
+  $Dcost = 69000;
+  $Pcost = 12000;
+  $Mcost = 20000;
+
+  $total_cost13 = $Mcost + $Mcost * 0.1;
+  $total_cost14 = $Mcost + $Mcost * 0.15;
+  $total_cost15 = $Mcost + $Mcost * 0.20;
+
+  $Expendituresumcost = $Lcost + $Ccost + $Dcost + $Pcost + $Mcost;
+  $Expendituresum10 = $total_cost1 + $total_cost4 + $total_cost7 + $total_cost10 + $total_cost13;
+  $Expendituresum15 = $total_cost2 + $total_cost5 + $total_cost8 + $total_cost11 + $total_cost14;
+  $Expendituresum20 = $total_cost3 + $total_cost6 + $total_cost9 + $total_cost12 + $total_cost15;		
+  ?>
+
     
 </body>
 </html>

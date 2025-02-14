9d85ac37683cfac8b9f37f77f64ab6f058dc0306
ashleyjanevillamor <villamor.ashleyjane@ici.edu.ph>
2025-02-14 18:13:22 +0800

    html table

--- PT2_VILLAMOR.php
+++ PT2_VILLAMOR.php
@@ -124,6 +124,18 @@ p {
   $Expendituresum20 = $total_cost3 + $total_cost6 + $total_cost9 + $total_cost12 + $total_cost15;		
   ?>
 
+<h1>Public Library Expansion Project</h1>
+  <h3>Cost Estimates</h3>
+<table bgcolor= "#63759c">
+  <tr>
+    <th>Expenditures</th>
+    <th>Estimated Cost</th>
+    <th>10% Increase</th>
+    <th>15% Increase</th>
+    <th>20% Increase</th>
+  </tr>
+</table>
+
     
 </body>
 </html>

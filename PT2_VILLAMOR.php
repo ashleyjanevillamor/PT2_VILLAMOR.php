2bf1a497cf6137854f06ec444c9f7b0f65ac1065
ashleyjanevillamor <villamor.ashleyjane@ici.edu.ph>
2025-02-14 18:11:33 +0800

    adding css

---
+++ PT2_VILLAMOR.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ExpansionProject.php</title>
+    <style>
+      h1 {
+    text-align: center;
+    color: #5599ee;
+    font-family: sans-serif;
+  }
+   
+h3 {
+  text-align: center;
+    color: gray;
+  font-family: sans-serif;
+}
+
+table {
+  table-layout: fixed;
+  width: 70%;
+  border: 0px solid #FFFFFF;
+  margin-left: auto; 
+  margin-right: auto;
+  height:40px;
+  font-family: sans-serif;
+ }
+   
+body {
+  background-color: #e0c7fd;
+  border: 1px solid;
+  border-width: 5px;
+}	
+   
+p {
+  color: black;
+  text-align: right;
+  font-weight: bold;
+  font-family: sans-serif;
+}
+
+.tb {
+  width: 70%;
+  margin-left: auto; 
+  margin-right: auto;
+  height: 40px;
+  font-weight: bold;
+  text-align: center;
+  font-family: sans-serif;
+}
+        </style>
+</head>
+<body>
+
+    
+</body>
+</html>
